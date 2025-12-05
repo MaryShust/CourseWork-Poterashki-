4e17c067d6a98c4777199ddbf53d2b24c740c379
MaryShust <maryshust00@gmail.com>
2025-12-05 20:27:25 +0300

    all front part

--- src/main/java/coursework/poterashki/controller/SPAController.java
+++ src/main/java/coursework/poterashki/controller/SPAController.java
@@ -1,4 +1,4 @@
-package coursework.poterashki;
+package coursework.poterashki.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
