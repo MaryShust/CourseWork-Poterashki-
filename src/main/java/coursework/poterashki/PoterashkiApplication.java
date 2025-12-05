0df26120aff82fe858eca0ab3df173934d533ddd
MaryShust <maryshust00@gmail.com>
2025-12-05 19:22:10 +0300

    init

---
+++ src/main/java/coursework/poterashki/PoterashkiApplication.java
@@ -0,0 +1,13 @@
+package coursework.poterashki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoterashkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoterashkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coursework/poterashki/PoterashkiApplicationTests.java
@@ -0,0 +1,13 @@
+package coursework.poterashki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoterashkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
