e2fe82a6c08e324202a23fafff8ee891c6809260
MaryShust <maryshust00@gmail.com>
2025-12-05 19:34:05 +0300

    add dependencies

---
+++ src/main/java/coursework/poterashki/SPAController.java
@@ -0,0 +1,24 @@
+package coursework.poterashki;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SPAController {
+
+    // Все SPA маршруты перенаправляем на index.html
+    @RequestMapping(value = {
+            "/",
+            "/dashboard",
+            "/login",
+            "/profile",
+            "/create",
+            "/edit/*",
+            "/my_announcements",
+            "/all_announcements",
+            "/details"
+    })
+    public String index() {
+        return "forward:/index.html";
+    }
+}
