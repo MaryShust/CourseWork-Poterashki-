d8739ab8f64f30309144c1df5da481489c910c27
m.s.taranenko <m.s.taranenko@tbank.ru>
2025-12-13 13:32:50 +0300

    backend-lost: добавить s3 поддержку

--- backend/lost/build.gradle.kts
+++ backend/lost/build.gradle.kts
@@ -5,4 +5,6 @@ dependencies {
     implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:${springCloudAwsVersion}"))
     implementation("io.awspring.cloud:spring-cloud-aws-starter-s3")
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+    implementation("io.minio:minio:8.6.0")
+
 }
--- backend/lost/src/main/kotlin/com/maxim/poteryashki/lost/LostApplication.kt
+++ backend/lost/src/main/kotlin/com/maxim/poteryashki/lost/LostApplication.kt
@@ -1,9 +1,13 @@
 package com.maxim.poteryashki.lost
 
+import com.maxim.poteryashki.lost.s3.MinioConfiguration
+import com.maxim.poteryashki.lost.s3.MinioProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(MinioProperties::class)
 class LostApplication
 
 fun main(args: Array<String>) {
